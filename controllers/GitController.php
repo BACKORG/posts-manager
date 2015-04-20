065f579b24d1b89986e50297b7e96a20078a0c74
Zhe Xiao <zhexiao@163.com>
2015-04-20 13:51:18 -0400

    git

--- controllers/GitController.php
+++ controllers/GitController.php
@@ -8,9 +8,8 @@ class GitController extends \yii\web\Controller{
     public function actionIndex(){
         $ref = Yii::$app->request->post('ref');
         print_r($ref);
-        
+
         exec("git pull", $output);
         var_dump($output);
     }
-
 }
