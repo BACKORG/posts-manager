a40761dce1d551d5c0125592c82e61a64380cd21
Zhe Xiao <zhexiao@163.com>
2015-04-20 13:50:50 -0400

    git

--- controllers/GitController.php
+++ controllers/GitController.php
@@ -6,6 +6,9 @@ class GitController extends \yii\web\Controller{
     public $enableCsrfValidation = false;
 
     public function actionIndex(){
+        $ref = Yii::$app->request->post('ref');
+        print_r($ref);
+        
         exec("git pull", $output);
         var_dump($output);
     }
