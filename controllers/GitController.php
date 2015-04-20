c008bfaee2e2ea132225809f8e23e95b46e97890
Zhe Xiao <zhexiao@163.com>
2015-04-20 12:03:18 -0400

    add git webhooks

--- controllers/GitController.php
+++ controllers/GitController.php
@@ -4,7 +4,6 @@ namespace app\controllers;
 
 class GitController extends \yii\web\Controller{
     public function actionIndex(){
-        echo '<pre>';
         print_r($_POST);
         return $this->render('index');
     }
