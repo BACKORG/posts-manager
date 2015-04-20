994d3d7fad0d665f37363f076cd4e133399fbf56
Zhe Xiao <zhexiao@163.com>
2015-04-20 13:27:25 -0400

    git

--- controllers/GitController.php
+++ controllers/GitController.php
@@ -6,6 +6,9 @@ class GitController extends \yii\web\Controller{
     public $enableCsrfValidation = false;
 
     public function actionIndex(){
+        $output = shell_exec('ls -lart');
+        echo "<pre>$output</pre>";
+        
         echo shell_exec("git pull");
     }
 
