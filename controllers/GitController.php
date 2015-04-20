8d468955f9d7709c4c7e696088d45027fe7098f1
Zhe Xiao <zhexiao@163.com>
2015-04-20 12:09:25 -0400

    add git webhooks

--- controllers/GitController.php
+++ controllers/GitController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 class GitController extends \yii\web\Controller{
+    public $enableCsrfValidation = false;
+    
     public function actionIndex(){
         print_r($_POST);
         return $this->render('index');
