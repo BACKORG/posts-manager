2d344104ae01ea19a753dee7de6fe6ddcca8a376
Zhe Xiao <zhexiao@163.com>
2015-05-07 00:44:49 -0400

    css

--- views/site/index.php
+++ views/site/index.php
@@ -2,6 +2,24 @@
 /* @var $this yii\web\View */
 $this->title = 'Post Manager';
 ?>
+
+<section id="tpl-intro">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12">
+                <div class="col-lg-6 col-md-6">
+                    <h1>Easy way to delete your posts</h1>
+                    <span class="heading-line"></span>
+                    <p>Give your a easy way to delete facebook, twitter, instagram, linkedin and any other social network posts.</p>
+                    <button type="button" class="btn btn-default intro-btn">Get it now</button>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <img src="/image/intro-mbp.png" class="img-responsive center-block" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <div id='site' ng-controller='SocialNetwork'>
     <ul class="site-s-n">
         <li ng-repeat="social in socialType">
